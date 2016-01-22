c900439baf4245bda6112a41e1d1915c9f66b9f7
patriciafig <patricia.figueroa@ucdenver.edu>
2016-01-22 13:00:09 -0700

    Revert "Update main.cpp"
    This reverts commit efb8c20c6a5b24dd48cab41576ccdbb1647bea9c.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world! My number is " << 100494907 << '!' << std::endl;
+    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
+    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
 
     return 0;
 }

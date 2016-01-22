35091f8dfd0f5e4aa72652d38d0d0c906e910799
patriciafig <patricia.figueroa@ucdenver.edu>
2016-01-22 13:49:51 -0700

    updated student id

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 /**
  * File: main.cpp
  * Project: ucd-csci2312-pa0
- * Author: Ivo Georgiev
+ * Author: Patricia Figueroa
  * Date: 2016-01-18
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    std::cout << "Hello, world! My number student ID number is " <<100494907<< '!' << std::endl;
 
     return 0;
 }
